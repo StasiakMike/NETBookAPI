2519bf1788133cda060761f722bec697cd89bb49
Michal Stasiak <michalstasiak@Michals-MacBook-Air.local>
2022-02-24 12:10:10 +0000

    complete API

---
+++ BookAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BookAPI.Model
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
